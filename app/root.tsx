b9f6d13ef9d15d357ccd2e285eac598ad30668c7
Milton Amaya <blurrdinfo@Gmail.com>
2025-08-07 16:17:44 -0700

    Update root.tsx to use proper Remix v2 hydration setup

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ export const links: LinksFunction = () => [
   },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export default function App() {
   return (
     <html lang="en">
       <head>
@@ -32,14 +32,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
