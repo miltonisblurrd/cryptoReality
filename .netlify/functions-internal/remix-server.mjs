d1e60e488fa00a05e46295a29f09bd13260ba417
Milton Amaya <blurrdinfo@Gmail.com>
2025-08-07 16:47:10 -0700

    Fix Netlify function to use proper Remix server handler

--- .netlify/functions-internal/remix-server.mjs
+++ .netlify/functions-internal/remix-server.mjs
@@ -1,10 +1,15 @@
+import { createRequestHandler } from "@remix-run/netlify";
+import * as serverBuild from "../../build/server/index.js";
 
-    export { default } from "../../build/server/index.js";
+export const handler = createRequestHandler({
+  build: serverBuild,
+  mode: process.env.NODE_ENV,
+});
 
-    export const config = {
-      name: "Remix server handler",
-      generator: "@netlify/remix-adapter@2.6.1",
-      path: "/*",
-      preferStatic: true,
-    };
+export const config = {
+  name: "Remix server handler",
+  generator: "@netlify/remix-adapter@2.6.1",
+  path: "/*",
+  preferStatic: true,
+};
     
