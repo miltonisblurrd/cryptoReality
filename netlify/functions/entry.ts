4b66f76d42f3ed80787b3e442d5ce73103ad1bf6
Milton Amaya <blurrdinfo@Gmail.com>
2025-08-07 16:05:20 -0700

    Simplify deployment following Netlify guide - use SPA configuration

--- netlify/functions/entry.ts
+++
@@ -1,9 +0,0 @@
-import { createRequestHandler } from "@remix-run/netlify";
-
-// Import the build from the build directory
-const build = require("../../build/server/index.js");
-
-export const handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-}); 
