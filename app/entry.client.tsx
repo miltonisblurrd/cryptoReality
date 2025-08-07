96c8872500665da69730f42d28aef30342a27a52
Milton Amaya <blurrdinfo@Gmail.com>
2025-08-07 16:58:23 -0700

    Fix hydration error by updating entry files and upgrading Remix packages

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,14 +5,6 @@
  */
 
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>
-  );
-});
+hydrateRoot(document, <RemixBrowser />);
