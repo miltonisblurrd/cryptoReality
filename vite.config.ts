6de105a7e2ff70b0b43ef61956a09db24d01ceba
Milton Amaya <blurrdinfo@Gmail.com>
2025-08-07 17:19:22 -0700

    Upgrade to Remix v2 and restore future configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,15 @@ import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 export default defineConfig({
   plugins: [
-    remix(),
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
+      },
+    }),
     tsconfigPaths(),
     netlifyPlugin(),
   ],
